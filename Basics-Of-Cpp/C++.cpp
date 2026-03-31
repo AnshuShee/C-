0dc22399fb723514c0bf57626c368b0a4bddac3a
Anshu Shee <anshu.shee.cg@gmail.com>
2026-03-31 10:21:13 +0530

    Add Basics-Of-Cpp/C++.cpp

---
+++ Basics-Of-Cpp/C++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+    cout<<"Hello world";
+    cout<<"\n";
+    cout<<"\n";
+    cout<<"Hello Everyone"<<endl;
+    cout<<"4+3"<<endl;
+    cout<<(4*3)<<endl;
+    int x,y;
+    x=5;
+    y=4;
+    cout<<x+y;
+
+}
